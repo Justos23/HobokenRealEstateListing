c1450bb3f5b3cfe1be65cea45c95baae78c1b80d
leiserrj <55596658+leiserrj@users.noreply.github.com>
2021-08-18 10:27:15 -0400

    Filters

--- routes/properties.js
+++ routes/properties.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const router = express.Router();
 const data = require('../data');
+const mongoCollections = require('../config/mongoCollections');
+const properties = mongoCollections.properties;
 const propertiesData = data.properties; 
 
 router.get('/filters', async (req, res) =>{
@@ -9,6 +12,32 @@ router.get('/filters', async (req, res) =>{
     })
 })
 
-
+router.post('/list',async (req, res) => {
+    const {sellType, homeType, price, numofBedrooms, numofBathrooms, squareFeet, location} = req.body;
+    const propertyDb = await properties();
+    let filtered = propertyDb.find();
+    if (sellType) {
+        filtered = filtered.find({ $elemMatch : { sellType : sellType} })
+    }
+    if (homeType) {
+        filtered = filtered.find({ $elemMatch : { homeType : homeType} })
+    }
+    if (price) {
+        filtered = filtered.find({ $elemMatch : { price : price} })
+    }
+    if (numofBedrooms) {
+        filtered = filtered.find({ $elemMatch : { numofBedrooms : numofBedrooms} })
+    }
+    if (numofBathrooms) {
+        filtered = filtered.find({ $elemMatch : { numofBathrooms : numofBathrooms} })
+    }
+    if (squareFeet) {
+        filtered = filtered.find({ $elemMatch : { squareFeet : squareFeet} })
+    }
+    if (location) {
+        filtered = filtered.find({ $elemMatch : { location : location} })
+    }
+    res.render('/properties/list',{properties: filtered});
+})
 
 module.exports = router; 
