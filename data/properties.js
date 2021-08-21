f2f13f5267323a4d4be65174b72926beffffc73b
Justos23 <justin.depardieu@gmail.com>
2021-08-21 19:37:50 +0200

    Fixed newproperties

--- data/properties.js
+++ data/properties.js
@@ -16,45 +16,49 @@ const exportedMethods = {
         return property;
       },
 
-    async CreateProperty(userID, sellType, homeType, price, numofBedrooms, numofBathrooms, squareFeet, streetname) {
+      async CreateProperty(userID, title, sellType, homeType, price, numofBedrooms, numofBathrooms, squareFeet, streetname, bio) {
 
-      if (!sellType) throw 'You need to state if the property is going to be either to sell or rent';
-      if (!homeType) throw 'You need to provide a valid home type';
-      if (!price) throw 'You need to provide a valid price';
-      if (!numofBedrooms) throw 'You need to provide a valid number of bedrooms';
-      if (!numofBathrooms) throw 'You need to provide a valid number of bathrooms';
-      if (!squareFeet) throw 'You need to provide a valid square feet';
-      if (!streetname || typeof streetname !== 'string' || !streetname.trim() ) throw 'You need to provide a valid location';
-
-        const propertyCollection = await properties();
-    
-        const newProperty = {
-            sellType:sellType,
-            homeType: homeType,
-            price: price,
-            numofBedrooms: numofBedrooms,
-            numofBathrooms: numofBathrooms,
-            squareFeet: squareFeet,
-            //address: address,
-            //pictures: [],
-            comments: [],
-            streetname: streetname,
-            Seller: userID,
-          }
-        
-    
-        const newInsertInformation = await propertyCollection.insertOne(newProperty);
-        const newId = newInsertInformation.insertedId;
-    
-        const userCollection = await users();
-        const updateInfo = await userCollection.updateOne({ username: userID},{$push: { 'Properties': newProperty }});
-          if (updateInfo.modifiedCount === 0) {
-              throw `Could not add property successfully`;
-          }
-        CurrentUser = await userCollection.findOne({ username: userID});
-        return CurrentUser;
-        //return await this.ReadPropertyById(newId);
-      },
+        if (!title) errors.push('You need to provide a title for your property');
+        if (!sellType) throw 'You need to state if the property is going to be either to sell or rent';
+        if (!homeType) throw 'You need to provide a valid home type';
+        if (!price) throw 'You need to provide a valid price';
+        if (!numofBedrooms) throw 'You need to provide a valid number of bedrooms';
+        if (!numofBathrooms) throw 'You need to provide a valid number of bathrooms';
+        if (!squareFeet) throw 'You need to provide a valid square feet';
+        if (!streetname || typeof streetname !== 'string' || !streetname.trim() ) throw 'You need to provide a valid location';
+        if (!bio) errors.push('You need to provide a bio for your property');
+  
+          const propertyCollection = await properties();
+      
+          const newProperty = {
+              title:title,
+              sellType:sellType,
+              homeType: homeType,
+              price: price,
+              numofBedrooms: numofBedrooms,
+              numofBathrooms: numofBathrooms,
+              squareFeet: squareFeet,
+              //address: address,
+              //pictures: [],
+              //comments: [],
+              streetname: streetname,
+              bio:bio,
+              Seller: userID,
+            }
+          
+      
+          const newInsertInformation = await propertyCollection.insertOne(newProperty);
+          const newId = newInsertInformation.insertedId;
+      
+          const userCollection = await users();
+          const updateInfo = await userCollection.updateOne({ username: userID},{$push: { 'Properties': newProperty }});
+            if (updateInfo.modifiedCount === 0) {
+                throw `Could not add property successfully`;
+            }
+          CurrentUser = await userCollection.findOne({ username: userID});
+          return CurrentUser;
+          //return await this.ReadPropertyById(newId);
+        },
 
     async addComment(propertyId,id) {
       const propertyDb = await properties();
--- data/users.js
+++ data/users.js
@@ -2,8 +2,8 @@ const { ObjectId } = require('mongodb');
 const mongoCollections = require('../config/mongoCollections');
 const comments = mongoCollections.comments;
 const users = mongoCollections.users;
-const bcrypt = require('bcrypt');
-//const bcrypt = require('bcryptjs');
+//const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 16;
 
 const exportedMethods = {
